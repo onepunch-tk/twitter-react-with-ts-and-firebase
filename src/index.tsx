1102fa49270c1b538fcf38d95b4e270b9275923f
TaekyungHa <86tkstar@gmail.com>
2022-10-05 18:03:58 +0900

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { QueryClient } from 'react-query/types/core';
+import { QueryClientProvider } from 'react-query/types/react';
+import GlobalStyle from "./styled/globalStyled";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+        <QueryClientProvider client={queryClient}></QueryClientProvider>
+        <GlobalStyle />
+        <App />
+    </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
